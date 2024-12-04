8dd2ae7f0bf7d79660cc72872230af9f1879861f
ErrEBalerdi <ricardobalerdi07122000@gmail.com>
2024-12-04 20:10:09 -0300

    mejorsas

---
+++ botones.js
@@ -0,0 +1,15 @@
+document.getElementById("btnCatalogo").addEventListener("click", function() {
+    window.location.href = "Catalogo.html";
+});
+
+document.getElementById("quienesSomos").addEventListener("click", function() {
+    window.location.href = "QuienesSomos.html";
+});
+
+document.getElementById("Contacto").addEventListener("click", function() {
+    window.location.href = "Contacto.html";
+});
+
+document.getElementById("btnInicio").addEventListener("click", function() {
+    window.location.href = "index.html";
+});
