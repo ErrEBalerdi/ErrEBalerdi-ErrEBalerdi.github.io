7467e923fd29e818a59c12c3c20f88c70dd55bd8
ErrEBalerdi <ricardobalerdi07122000@gmail.com>
2024-12-05 13:34:02 -0300

    arreglos nuevos

--- botonesCatalogo.js
+++ botonesCatalogo.js
@@ -1,13 +1,11 @@
-
-// botones inicio
-document.getElementById("btnCatalogo").addEventListener("click", function() {
-    window.location.href = "Catalogo.html";
+ //botones catalogo
+ document.getElementById("Inicioboton").addEventListener("click", function() {
+    window.location.href = "Index.html";
   });
-  
+    
   document.getElementById("quienesSomos").addEventListener("click", function() {
     window.location.href = "QuienesSomos.html";
   });
-  
   document.getElementById("Contacto").addEventListener("click", function() {
     window.location.href = "Contacto.html";
   });
---
+++ botonesContacto.js
@@ -0,0 +1,11 @@
+document.getElementById("Inicioboton").addEventListener("click", function() {
+    window.location.href = "Index.html";
+  });
+    
+  document.getElementById("quienesSomos").addEventListener("click", function() {
+    window.location.href = "QuienesSomos.html";
+  });
+  document.getElementById("btnCatalogo").addEventListener("click", function() {
+    window.location.href = "Catalogo.html";
+  });
+ 
